7508f1b3181570ce12cf994ee4934ddad7a25ebb
Jens Duray <bobaflex.hsv@gmail.com>
2020-04-30 22:26:16 +0200

    implement test for PokemonDetails

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -2,7 +2,7 @@ import React,{useState,useEffect} from 'react';
 import {useParams} from 'react-router'
 
 export default() =>{
-  let {id, info} = useParams();
+  let {id,info} = useParams();
   console.log(useParams());
   return(
     <div>
